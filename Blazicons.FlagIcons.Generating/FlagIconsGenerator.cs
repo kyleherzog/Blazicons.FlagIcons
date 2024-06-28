328a008dab701d851be05caed0acb2cf827d6c63
Kyle Herzog <holler@kyleherzog.com>
2024-06-28 13:03:17 -0500

    Initial Commit

---
+++ Blazicons.FlagIcons.Generating/FlagIconsGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Blazicons.Generating;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.Threading;
+
+namespace Blazicons.FlagIcons.Generating;
+
+[Generator]
+internal class FlagIconsGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        using var taskContext = new JoinableTaskContext();
+        var taskFactory = new JoinableTaskFactory(taskContext);
+        var downloader = new RepoDownloader(new Uri(Properties.Resources.DownloadAddress));
+        taskFactory.Run(
+            async () =>
+            {
+                await downloader.Download(@"^flags\/.*\.svg$").ConfigureAwait(true);
+            });
+
+        var svgFolder = Path.Combine(downloader.ExtractedFolder, $"{downloader.RepoName}-{downloader.BranchName}", "flags", "4x3");
+        context.WriteIconsClass("FlagIcons4x3", svgFolder);
+
+        svgFolder = Path.Combine(downloader.ExtractedFolder, $"{downloader.RepoName}-{downloader.BranchName}", "flags", "1x1");
+        context.WriteIconsClass("FlagIcons1x1", svgFolder);
+
+        downloader.CleanUp();
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // required by ISourceGenerator
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Blazicons.FlagIcons
+Provides the Flag Icons library packaged as [Blazicons](https://github.com/kyleherzog/Blazicons), SVG icon components for Blazor.
+
+Check out the [Demo Site](http://blazicons.com).
+
+![Nuget](https://img.shields.io/nuget/v/Blazicons.FlagIcons)
+
+[![Build Status](https://dev.azure.com/kyleherzog/Blazicons/_apis/build/status/Blazicons.FlagIcons?branchName=main)](https://dev.azure.com/kyleherzog/Blazicons.FlagIcons/_build/latest?definitionId=18&branchName=main)
+
+## Getting Started
+To get started using the Flag Icons Blazicons, just install the Blazicons.FlagIcons NuGet package.
+
+Next add the Blazicons reference to the `_Imports.razor` file in the Blazor project.
+
+```csharp
+@using Blazicons
+```
+
+Finally, add the Blazicon component to your Blazor pages/components.
+```html
+<Blazicon Svg="FlagIcons4x3.Us"></Blazicon>
+<Blazicon Svg="FlagIcons1x1.Us"></Blazicon>
+```
+
+## Parameters & Styling
+See the [Blazicons](https://github.com/kyleherzog/Blazicons) documentation for details on parameters and styling.
+
+## Credits
+Thanks to the creators of [Flag Icons](https://github.com/lipis/flag-icons)
